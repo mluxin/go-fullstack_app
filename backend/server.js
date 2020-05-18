fb602981e8127841209eda5a0ad399dff2727c7a
Rinaroar <marina.luxin@gmail.com>
2020-05-18 15:59:14 +0200

    linked app.js (express) w/ server.js (node server)

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,8 @@
 const http = require('http');
+const app = require('./app');
 
-const server = http.createServer((req, res) => {
-    res.end('Voilà la réponse du serveur !');
-});
+app.set('port', process.env.PORT || 3000);
+const server = http.createServer(app);
 
 // if environnenment 3000 by default is not available
 server.listen(process.env.PORT || 3000);
