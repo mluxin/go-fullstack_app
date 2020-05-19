e550fa1764a729a6b8df43b8992238138a97d899
Rinaroar <marina.luxin@gmail.com>
2020-05-19 15:50:52 +0200

    optimize with routes

--- backend/app.js
+++ backend/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
-const Thing = require('./models/thing');
+const stuffRoutes = require('./routes/stuff');
 
 mongoose.connect('mongodb+srv://johndoe:johndoe12345@cluster0-lcnd1.mongodb.net/test?retryWrites=true&w=majority',
   { useNewUrlParser: true,
@@ -23,40 +23,6 @@ app.use((req, res, next) => {
 // .use is for all the routes and json() is a method of body-parser object
 app.use(bodyParser.json());
 
-app.post('/api/stuff', (req, res, next) => {
-  delete req.body._id;
-  const thing = new Thing({
-    ...req.body // copy fields in body request
-  });
-  //register object in DB and return a promise
-  thing.save()
-  .then(() => res.status(201).json({ message : 'Objet enregistré !' }))
-  .catch(error => res.status(400).json({ error:error }));
-});
-
-app.put('/api/stuff/:id', (req, res, next) => {
-  Thing.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
-  .then(() => res.status(201).json({ message : 'Objet modifié !' }))
-  .catch(error => res.status(400).json({ error:error }));
-})
-
-app.delete('/api/stuff/:id', (req, res, next) => {
-  Thing.deleteOne({ _id: req.params.id})
-  .then(() => res.status(201).json({ message : 'Objet supprimé !' }))
-  .catch(error => res.status(400).json({ error:error }));
-})
-
-app.get('/api/stuff/:id', (req, res, next) => {
-  Thing.findOne({ _id: req.params.id})
-  .then(thing => res.status(200).json(thing))
-  .catch(error => res.status(404).json({ error }));
-});
-
-
-app.get('/api/stuff', (req, res, next) => {
-  Thing.find()
-  .then( things => res.status(200).json(things))
-  .catch(error => res.status(400).json({ error:error }));
-});
+app.use('/api/stuff', stuffRoutes);
 
 module.exports = app;
